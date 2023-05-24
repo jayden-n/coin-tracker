b720667b6fc98b5ef003eaedbffe6dc37401a8ee
jayden-n <jaydennguyen.dev@gmail.com>
2023-05-24 01:17:05 -0400

    added table container from material ui

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 import Banner from '../components/Banner/Banner';
+import CoinsTable from '../components/CoinsTable';
 const Homepage = () => {
   return (
     <Fragment>
       <Banner />
+      <CoinsTable />
     </Fragment>
   );
 };
---
+++ src/components/CoinsTable.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react';
+import { CoinList } from '../config/api';
+import axios from 'axios';
+import { CryptoState } from '../CryptoContext';
+import {
+  Container,
+  LinearProgress,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from '@material-ui/core';
+const CoinsTable = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState('');
+
+  const { currency } = CryptoState();
+
+  const fetchCoins = async () => {
+    setLoading(true);
+
+    const { data } = await axios.get(CoinList(currency));
+    setCoins(data);
+    setLoading(false);
+  };
+
+  console.log(coins);
+  useEffect(() => {
+    fetchCoins();
+  }, [currency]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#fff',
+      },
+      type: 'dark',
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container style={{ textAlign: 'center' }}>
+        <Typography
+          variant="h4"
+          style={{
+            margin: 18,
+            fontFamily: 'Montserrat',
+          }}
+        >
+          Cryptocurrency Prices by Market Cap
+        </Typography>
+        <TextField
+          label="Search for a crypto currency..."
+          variant="outlined"
+          style={{ marginBottom: 20, width: '100%' }}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <TableContainer>
+          {loading ? (
+            <LinearProgress style={{ backgroundColor: 'gold' }} />
+          ) : (
+            <Table>
+              <TableHead style={{ backgroundColor: '#EEBC1D' }}>
+                <TableRow>
+                  {['Coin', 'Price', '24h Change', 'Market Cap'].map((head) => (
+                    <TableCell
+                      style={{
+                        color: 'black',
+                        fontWeight: '700',
+                        fontFamily: 'Montserrat',
+                      }}
+                      key={head}
+                      align={head === 'Coin' ? '' : 'right'}
+                    >
+                      {head}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+            </Table>
+          )}
+        </TableContainer>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default CoinsTable;
