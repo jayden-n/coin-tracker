31c89d4fca476aa9ff7f801020b2871fa5ac15fb
jayden-n <jaydennguyen.dev@gmail.com>
2023-05-24 03:04:40 -0400

    added historical chart component with time buttons

--- src/components/Banner/Carousel.jsx
+++ src/components/Banner/Carousel.jsx
@@ -41,6 +41,7 @@ const Carousel = () => {
   console.log(trending);
   useEffect(() => {
     fetchTrendingCoins();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currency]);
 
   const items = trending.map((coin) => {
