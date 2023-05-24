f1660a2a75f70421d71fc0dc4bc38c6dfd6f68c9
jayden-n <jaydennguyen.dev@gmail.com>
2023-05-23 23:21:59 -0400

    added context api

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -1,8 +1,28 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 const Crypto = createContext();
+
 const CryptoContext = ({ children, value }) => {
-  return <Crypto.Provider value={value}>{children}</Crypto.Provider>;
+  const [currency, setCurrency] = useState('USD');
+  const [symbol, setSymbol] = useState('$');
+
+  useEffect(() => {
+    if (currency === 'USD') {
+      setSymbol('$');
+    } else if (currency === 'VND') {
+      setSymbol('₫');
+    }
+  }, [currency]);
+
+  return (
+    <Crypto.Provider value={{ currency, symbol, setCurrency }}>
+      {children}
+    </Crypto.Provider>
+  );
 };
 
 export default CryptoContext;
+
+export const CryptoState = () => {
+  return useContext(Crypto);
+};
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,7 @@ import {
 } from '@material-ui/core';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { CryptoState } from '../CryptoContext';
 
 const useStyles = makeStyles(() => ({
   title: {
@@ -25,7 +26,7 @@ const useStyles = makeStyles(() => ({
 const Header = () => {
   const classes = useStyles();
   const navigate = useNavigate();
-
+  const { currency, setCurrency } = CryptoState();
   const darkTheme = createTheme({
     palette: {
       primary: {
@@ -53,6 +54,8 @@ const Header = () => {
                 height: 40,
                 marginRight: 15,
               }}
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value)}
             >
               <MenuItem value={'USD'}>USD</MenuItem>
               <MenuItem value={'VND'}>VND</MenuItem>
